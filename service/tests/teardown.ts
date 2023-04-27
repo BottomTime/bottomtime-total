83530c75ae00ae4c9d3b1f52333af7632c055722
Chris Carleton <mrchriscarleton@gmail.com>
2023-04-27 10:51:45 -0400

    Using dedicated Docker image for MongoDb in CI.

--- service/tests/teardown.ts
+++ service/tests/teardown.ts
@@ -2,8 +2,11 @@
 import { type MongoMemoryServer } from 'mongodb-memory-server';
 
 export default async function (): Promise<void> {
-  console.log('\nStopping MongoDB server...');
   const mongod: MongoMemoryServer = (globalThis as any).mongod;
-  await mongod.stop({ doCleanup: true });
-  console.log('MongoDB server has stopped.');
+
+  if (mongod) {
+    console.log('\nStopping MongoDB server...');
+    await mongod.stop({ doCleanup: true });
+    console.log('MongoDB server has stopped.');
+  }
 }
