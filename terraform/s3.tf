c279e01f459015b76e7158090b0bacb3a2f6c101
Chris Carleton <mrchriscarleton@gmail.com>
2023-04-28 15:50:39 -0400

    Fixing Terraform deploy errors and attempting distribution invalidation.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -8,11 +8,6 @@ resource "aws_s3_bucket" "web" {
   }
 }
 
-resource "aws_s3_bucket_acl" "web" {
-  bucket = aws_s3_bucket.web.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket" "docs" {
   bucket        = "${var.service_name_short}-${data.aws_region.current.name}-${var.env}-docs-distro"
   force_destroy = true
@@ -21,9 +16,4 @@ resource "aws_s3_bucket" "docs" {
     Name        = "${var.service_name} Docs Distro Bucket"
     Environment = var.env
   }
-}
-
-resource "aws_s3_bucket_acl" "docs" {
-  bucket = aws_s3_bucket.docs.id
-  acl    = "private"
 }
