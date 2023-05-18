85f523a56a396c4354ec9ae55caac0fd27d2923b
Chris Carleton <mrchriscarleton@gmail.com>
2023-05-18 07:00:25 -0400

    Added VPC end points to save 706.

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -102,3 +102,52 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private[count.index].id
   subnet_id      = aws_subnet.private[count.index].id
 }
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = aws_route_table.private.*.id
+  # TODO
+  policy = jsonencode({
+
+  })
+}
+
+resource "aws_vpc_endpoint" "ecr-dkr" {
+  vpc_id              = aws_vpc.main.id
+  private_dns_enabled = true
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [aws_security_group.task.id]
+  subnet_ids          = aws_subnet.private.*.id
+}
+
+resource "aws_vpc_endpoint" "ecr-api" {
+  vpc_id              = aws_vpc.main.id
+  private_dns_enabled = true
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [aws_security_group.task.id]
+  subnet_ids          = aws_subnet.private.*.id
+}
+
+resource "aws_vpc_endpoint" "ecs-agent" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ecs-agent"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  security_group_ids  = [aws_security_group.task.id]
+  subnet_ids          = aws_subnet.private.*.id
+
+
+}
+
+resource "aws_vpc_endpoint" "ecs-telemetry" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ecs-telemetry"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  security_group_ids  = [aws_security_group.task.id]
+  subnet_ids          = aws_subnet.private.*.id
+}
