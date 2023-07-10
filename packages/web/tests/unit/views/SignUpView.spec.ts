6e62242237cc9d2d183958305dcedcc86fcad116
Chris Carleton <mrchriscarleton@gmail.com>
2023-07-10 08:34:00 -0400

    Signup page view is tested.

--- packages/web/tests/unit/views/SignUpView.spec.ts
+++ packages/web/tests/unit/views/SignUpView.spec.ts
@@ -1,32 +1,26 @@
-import { mount } from '@vue/test-utils';
-import request, { SuperAgentStatic } from 'superagent';
-
-import { createErrorHandler } from '@/helpers';
-import { createStore } from '@/store';
-import { DefaultUser } from '@/users';
-import { fakeUser } from '../../fixtures/fake-user';
-import LoginView from '@/views/LoginView.vue';
 import { Mock } from 'moq.ts';
+import { mount } from '@vue/test-utils';
+import request from 'superagent';
+
+import { createStore } from '@/store';
+import { fakeUser } from '../../fixtures/fake-user';
+import { initialStoreState } from '../../fixtures/store-state';
+import SignUpView from '@/views/SignupView.vue';
 import {
   StoreKey,
   UserManagerKey,
   WithErrorHandlingKey,
 } from '@/injection-keys';
-import { initialStoreState } from '../../fixtures/store-state';
-import { UserManager } from '@/users';
+import { DefaultUser, UserManager } from '@/users';
+import { createErrorHandler } from '@/helpers';
 
-describe('Login View', () => {
+describe('Sign Up Page', () => {
   const userManager = new Mock<UserManager>().object;
-  let agent: SuperAgentStatic;
 
-  beforeAll(() => {
-    agent = request.agent();
-  });
-
-  it('Will display login form if user is currently browsing anonymously', async () => {
+  it('Will display the signup form if the user is anonymous', async () => {
     const store = createStore(initialStoreState());
     const errorHandler = createErrorHandler(store);
-    const wrapper = mount(LoginView, {
+    const wrapper = mount(SignUpView, {
       global: {
         provide: {
           [StoreKey as symbol]: store,
@@ -35,19 +29,19 @@ describe('Login View', () => {
         },
       },
     });
-    expect(wrapper.find('div#login-page').isVisible()).toBe(true);
+    expect(wrapper.find('div#signup-page').isVisible()).toBe(true);
     expect(wrapper.find('div#user-logged-in-content').exists()).toBe(false);
   });
 
-  it('Will show a helpful message if user is already logged in', async () => {
+  it('Will show a helpful message if the user is already logged in', async () => {
     const userData = fakeUser();
     const store = createStore(
       initialStoreState({
-        currentUser: new DefaultUser(agent, userData),
+        currentUser: new DefaultUser(request.agent(), userData),
       }),
     );
     const errorHandler = createErrorHandler(store);
-    const wrapper = mount(LoginView, {
+    const wrapper = mount(SignUpView, {
       global: {
         provide: {
           [StoreKey as symbol]: store,
@@ -56,7 +50,7 @@ describe('Login View', () => {
         },
       },
     });
-    expect(wrapper.find('div#login-page').exists()).toBe(false);
+    expect(wrapper.find('div#signup-page').exists()).toBe(false);
     expect(wrapper.find('div#user-logged-in-content').isVisible()).toBe(true);
   });
 });
