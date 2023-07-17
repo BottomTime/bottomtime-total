6f989d55a25b0efdc58a30981176f9c2a18af98a
Chris Carleton <mrchriscarleton@gmail.com>
2023-07-17 13:10:24 -0400

    Updated data models for dive sites.

--- packages/service/src/data/dive-site-document.ts
+++ packages/service/src/data/dive-site-document.ts
@@ -1,6 +1,34 @@
 import { Document } from 'mongodb';
 
+export interface DiveSiteReviewDocument extends Document {
+  _id: string;
+  creator: string;
+  createdOn: Date;
+  updatedOn?: Date;
+  rating: number;
+  title: string;
+  comments?: string;
+}
+
 export interface DiveSiteDocument extends Document {
   _id: string;
+  creator: string;
+  createdOn: Date;
+  updatedOn?: Date;
+
   name: string;
+  description?: string;
+
+  location: string;
+  directions?: string;
+  gps?: {
+    type: 'Point';
+    coordinates: [number, number];
+  };
+
+  freeToDive?: boolean;
+  shoreAccess?: boolean;
+
+  averageRating: number;
+  reviews?: DiveSiteReviewDocument[];
 }
