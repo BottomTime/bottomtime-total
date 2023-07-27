49df0d24becf64315ad41a41e949abfd0152a86f
Chris Carleton <mrchriscarleton@gmail.com>
2023-07-26 21:10:15 -0400

    Added admin tools with a command for creating tokens.

---
+++ packages/service/admin/bt-admin.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env ts-node-script
+import yargs from 'yargs';
+import { getUserToken } from './get-user-token';
+
+require('dotenv-defaults').config();
+
+async function processCommand(cmd: string[]) {
+  await yargs(cmd)
+    .version('1.0.0')
+    .command(
+      'token <user>',
+      'Gets a token for a user',
+      async (yargs) => {
+        return await yargs
+          .positional('user', {
+            demandOption: true,
+            description: "The user's username or email address.",
+            type: 'string',
+          })
+          .help();
+      },
+      async (yargs) => {
+        await getUserToken(yargs.user);
+      },
+    )
+    .help().argv;
+}
+
+processCommand(process.argv.slice(2))
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
