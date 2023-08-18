397c37ce1f968eea98eb14e9a33734311c3ee957
Chris Carleton <mrchriscarleton@gmail.com>
2023-08-18 18:14:04 -0400

    Removed redundant Zod schema.

--- packages/service/src/common.ts
+++ packages/service/src/common.ts
@@ -1,17 +1,7 @@
-import { z } from 'zod';
-
 export interface GpsCoordinates {
   lat: number;
   lon: number;
 }
-
-// "Borrowed" from Zod documentation: https://github.com/colinhacks/zod#json-type
-const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()]);
-type Literal = z.infer<typeof literalSchema>;
-type Json = Literal | { [key: string]: Json } | Json[];
-export const JsonSchema: z.ZodType<Json> = z.lazy(() =>
-  z.union([literalSchema, z.array(JsonSchema), z.record(JsonSchema)]),
-);
 
 export interface Range {
   min: number;
