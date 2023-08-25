615d2f68c6efce2030e8870be8aed7ca666ae09a
Chris Carleton <mrchriscarleton@gmail.com>
2023-08-25 11:14:43 -0400

    Updated snapshots to fix failing tests.

--- packages/e2e-tests/tests/specs/dive-sites/searching.spec.ts
+++ packages/e2e-tests/tests/specs/dive-sites/searching.spec.ts
@@ -35,19 +35,73 @@ test.describe('Searching dive sites', () => {
     ]);
   });
 
-  test.afterEach(async ({ app }) => {
-    await app.purgeDatabase();
-  });
-
   test('Will display a list of sites on page load', async ({ page }) => {
+    const expected = [
+      'Ignorant Coyote',
+      'Ethical Savings',
+      'Extra-Small Wallaby',
+      'Gullible Oven',
+      'Foolish Familiarity',
+      'Inexperienced Scimitar',
+      'Flashy Desk',
+      'Sore Advocacy',
+      'Gloomy Cation',
+      'Each Breath',
+      'Illustrious Recording',
+      'Steep Hood',
+      'Shy Gaiters',
+      'Kooky Jam',
+      'Average Lesbian',
+      'Kindhearted Thought',
+      'Unwieldy Optimal',
+      'Competent Wildebeest',
+      'Crazy Sense',
+      'Wrathful Container',
+      'Quick-Witted Epithelium',
+      'Tempting Hate',
+      'Kindly Leprosy',
+      'Accomplished Icecream',
+      'Frivolous Grandchild',
+      'Infinite Subgroup',
+      'Biodegradable Chassis',
+      'Indelible Noise',
+      'Illustrious Anguish',
+      'Motionless Stamina',
+      'Qualified Beyond',
+      'Disloyal Apology',
+      'Celebrated Paint',
+      'Bad Proof',
+      'Jaunty Assignment',
+      'Piercing Sepal',
+      'Double Cop-Out',
+      'Slimy Cyst',
+      'Stale Meal',
+      'Acclaimed Windage',
+      'Unique Stamp',
+      'Careful Down',
+      'Wary Sweat',
+      'Similar Precision',
+      'Sympathetic Holder',
+      'Moist Soda',
+      'Academic Sanction',
+      'Inconsequential Volume',
+      'Drafty Firewall',
+      'Quizzical Cloth',
+    ];
+
     await page.goto('/diveSites');
     const sites = await page.getByTestId('dive-site-name').allInnerTexts();
     const count = await page.getByTitle('Site count').innerText();
     expect(count).toContain(sites.length.toString());
-    expect(sites.join(',')).toMatchSnapshot();
+    expect(sites).toEqual(expected);
   });
 
   test('Will perform a text search', async ({ page }) => {
+    const expected = [
+      'Biodegradable Chassis',
+      'Unwieldy Optimal',
+      'Hidden Metallurgist',
+    ];
     await page.goto('/diveSites');
 
     const searchBox = page.getByRole('searchbox');
@@ -58,17 +112,23 @@ test.describe('Searching dive sites', () => {
     const sites = await page.getByTestId('dive-site-name').allInnerTexts();
     const count = await page.getByTitle('Site count').innerText();
     expect(count).toContain(sites.length.toString());
-    expect(sites.join(',')).toMatchSnapshot();
+    expect(sites).toEqual(expected);
   });
 
   test('Will display a message if no dive sites can be found', async ({
     page,
   }) => {
-    await DiveSites.deleteMany({});
     await page.goto('/diveSites');
+
+    const searchBox = page.getByRole('searchbox');
+    await searchBox.type('definitelynotgoingtofindthissearchterm');
+    await searchBox.press('Enter');
+    await page.waitForLoadState('networkidle');
 
     const alert = page.getByRole('alert');
     await expect(alert).toBeVisible();
-    await expect(alert.innerText()).resolves.toMatchSnapshot();
+    await expect(alert.innerText()).resolves.toEqual(
+      'No dive sites were found matching your search parameters.',
+    );
   });
 });
