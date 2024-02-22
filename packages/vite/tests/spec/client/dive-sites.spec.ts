15ab9dbd3c0052fb1dbea09997d27ac436f234ee
Chris Carleton <mrchriscarleton@gmail.com>
2024-02-22 15:14:09 -0500

    Updated tests for API client.

--- packages/vite/tests/spec/client/dive-sites.spec.ts
+++ packages/vite/tests/spec/client/dive-sites.spec.ts
@@ -1,6 +1,8 @@
 import {
+  DiveSitesSortBy,
   SearchDiveSitesResponseDTO,
   SearchDiveSitesResponseSchema,
+  SortOrder,
 } from '@bottomtime/api';
 
 import axios, { AxiosInstance } from 'axios';
@@ -22,7 +24,36 @@ describe('Dive Site API client', () => {
     searchResults = SearchDiveSitesResponseSchema.parse(SearchResults);
   });
 
-  it('will perform a search for dive sites', () => {});
+  it('will perform a search for dive sites', async () => {
+    const spy = jest.spyOn(axiosClient, 'get').mockResolvedValue({
+      data: searchResults,
+    });
+
+    const result = await apiClient.searchDiveSites({
+      query: 'Puget Sound',
+      freeToDive: true,
+      shoreAccess: true,
+      difficulty: { min: 1, max: 3 },
+      location: {
+        lat: 47.6,
+        lon: -122.3,
+      },
+      radius: 250,
+      rating: { min: 3.5, max: 5 },
+      skip: 100,
+      limit: 200,
+      sortBy: DiveSitesSortBy.Rating,
+      sortOrder: SortOrder.Descending,
+      creator: 'bob',
+    });
+
+    expect(spy.mock.lastCall).toMatchSnapshot();
+    expect(result.totalCount).toBe(searchResults.totalCount);
+    expect(result.sites).toHaveLength(searchResults.sites.length);
+    expect(result.sites.map((site: DiveSite) => site.toJSON())).toEqual(
+      searchResults.sites,
+    );
+  });
 
   it('will perform a search with minimal options', async () => {
     const spy = jest.spyOn(axiosClient, 'get').mockResolvedValue({
@@ -31,13 +62,7 @@ describe('Dive Site API client', () => {
 
     const result = await apiClient.searchDiveSites();
 
-    expect(spy).toHaveBeenCalledWith('/api/diveSites', {
-      params: {
-        difficulty: undefined,
-        location: undefined,
-        rating: undefined,
-      },
-    });
+    expect(spy.mock.lastCall).toMatchSnapshot();
     expect(result.totalCount).toBe(searchResults.totalCount);
     expect(result.sites).toHaveLength(searchResults.sites.length);
     expect(result.sites.map((site: DiveSite) => site.toJSON())).toEqual(
