6f68a54b8eac5f5d7792965da750813e81af444b
Chris Carleton <mrchriscarleton@gmail.com>
2024-02-26 11:59:11 -0500

    Dive site page will render its components.

--- packages/vite/src/components/diveSites/view-dive-site.vue
+++ packages/vite/src/components/diveSites/view-dive-site.vue
@@ -1,3 +1,16 @@
 <template>
   <div>View Dive Site</div>
+  <p>
+    {{ JSON.stringify(site) }}
+  </p>
 </template>
+
+<script lang="ts" setup>
+import { DiveSiteDTO } from '@bottomtime/api';
+
+type ViewDiveSiteProps = {
+  site: DiveSiteDTO;
+};
+
+defineProps<ViewDiveSiteProps>();
+</script>
---
+++ packages/vite/tests/spec/views/dive-site-view.spec.ts
@@ -0,0 +1,87 @@
+import { ApiClient, ApiClientKey, DiveSite } from '@/client';
+import DiveSiteView from '@/views/dive-site-view.vue';
+import {
+  ComponentMountingOptions,
+  mount,
+  renderToString,
+} from '@vue/test-utils';
+
+import { Pinia, createPinia } from 'pinia';
+import { Router } from 'vue-router';
+
+import { createAxiosError } from '../../fixtures/create-axios-error';
+import { createRouter } from '../../fixtures/create-router';
+import { DiveSiteWithFullProperties } from '../../fixtures/sites';
+
+describe('Dive Site View', () => {
+  let client: ApiClient;
+  let router: Router;
+
+  let pinia: Pinia;
+  let opts: ComponentMountingOptions<typeof DiveSiteView>;
+
+  beforeAll(() => {
+    client = new ApiClient();
+    router = createRouter([
+      {
+        path: '/diveSites/:siteId',
+        name: 'DiveSite',
+        component: DiveSiteView,
+      },
+    ]);
+  });
+
+  beforeEach(async () => {
+    await router.push(`/diveSites/${DiveSiteWithFullProperties.id}`);
+    window.__INITIAL_STATE__ = {
+      currentUser: null,
+      currentDiveSite: DiveSiteWithFullProperties,
+    };
+    pinia = createPinia();
+    opts = {
+      global: {
+        plugins: [pinia, router],
+        provide: {
+          [ApiClientKey as symbol]: client,
+        },
+      },
+    };
+  });
+
+  it('will prefetch the site data on the server side', async () => {
+    const spy = jest
+      .spyOn(client.diveSites, 'getDiveSite')
+      .mockResolvedValue(
+        new DiveSite(client.axios, DiveSiteWithFullProperties),
+      );
+    const html = await renderToString(DiveSiteView, { global: opts.global });
+    expect(spy).toHaveBeenCalledWith(DiveSiteWithFullProperties.id);
+    expect(html).toMatchSnapshot();
+  });
+
+  it('will show not found message if prefetch cannot find the site on the server side', async () => {
+    window.__INITIAL_STATE__.currentDiveSite = null;
+    const error = createAxiosError({
+      status: 404,
+      message: 'Not Found',
+      method: 'GET',
+      path: `/api/diveSites/${DiveSiteWithFullProperties.id}`,
+    });
+    const spy = jest
+      .spyOn(client.diveSites, 'getDiveSite')
+      .mockRejectedValue(error);
+    const html = await renderToString(DiveSiteView, { global: opts.global });
+    expect(spy).toHaveBeenCalledWith(DiveSiteWithFullProperties.id);
+    expect(html).toMatchSnapshot();
+  });
+
+  it('will show not found message if site cannot be found', () => {
+    window.__INITIAL_STATE__.currentDiveSite = null;
+    const wrapper = mount(DiveSiteView, opts);
+    expect(wrapper.get('[data-testid="not-found-message"]').isVisible()).toBe(
+      true,
+    );
+  });
+
+  it.todo('will render the site info if the site is found');
+});
