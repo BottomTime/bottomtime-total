922c878457e32f7b26b6a97b7db2a464e97c10e1
Chris Carleton <mrchriscarleton@gmail.com>
2024-03-25 08:38:33 -0400

    Updated front-end tests to reflect removal of profileVisibility.

--- packages/web/src/click-outside.ts
+++ packages/web/src/click-outside.ts
@@ -1,15 +1,21 @@
-export const clickOutside = {
-  beforeMount: (el, binding) => {
+import { Directive, DirectiveBinding } from 'vue';
+
+type HTMLElementWithClickOutside = HTMLElement & {
+  clickOutsideEvent: (e: MouseEvent) => void;
+};
+
+export const clickOutside: Directive = {
+  beforeMount: (el: HTMLElementWithClickOutside, binding: DirectiveBinding) => {
     el.clickOutsideEvent = (event) => {
       // here I check that click was outside the el and his children
-      if (!(el == event.target || el.contains(event.target))) {
+      if (!(el == event.target || el.contains(event.target as Node))) {
         // and if it did, call method provided in attribute value
         binding.value();
       }
     };
     document.addEventListener('click', el.clickOutsideEvent);
   },
-  unmounted: (el) => {
+  unmounted: (el: HTMLElementWithClickOutside) => {
     document.removeEventListener('click', el.clickOutsideEvent);
   },
 };
