d3dfc148bff1c610a095355f51996ddd28b44bb8
Chris Carleton <mrchriscarleton@gmail.com>
2024-04-05 16:12:40 -0400

    New design for Change Avatar Dialog

--- packages/web/src/components/dialog/change-avatar-dialog.vue
+++ packages/web/src/components/dialog/change-avatar-dialog.vue
@@ -6,28 +6,32 @@
     @close="$emit('cancel')"
   >
     <template #default>
-      <div
-        class="flex flex-row gap-3 m-4 mb-3 p-2 rounded-md text-sm bg-link text-grey-900"
-      >
-        <span>
-          <i class="fas fa-info-circle fa-2x"></i>
-        </span>
-        <div class="grow">
-          <span class="font-bold">
-            TODO: I need to build out this dialog still but I need some
-            additional infrastructure first.
-          </span>
-          <ul class="list-inside list-disc">
-            <li>I need file storage for uploaded images.</li>
-            <li>I need a way to crop and center uploaded images.</li>
-            <li>I might accept URLs from trusted sources (e.g. Gravatar.)</li>
-          </ul>
+      <ImageCropper
+        v-if="avatarUrl"
+        :image="avatarUrl"
+        :target-width="512"
+        circle
+      />
+      <div v-else class="w-full flex flex-col space-y-3">
+        <div class="h-[200px] border-2 border-secondary rounded-lg flex">
+          <div class="m-auto text-center text-xl text-secondary space-x-3">
+            <span>
+              <i class="fa-regular fa-file-image"></i>
+            </span>
+            <span>Drag your image here.</span>
+          </div>
+        </div>
+
+        <div class="text-center">
+          <FormButton size="sm">Select File...</FormButton>
         </div>
       </div>
     </template>
 
     <template #buttons>
-      <FormButton type="primary" disabled @click="onSave"> Save </FormButton>
+      <FormButton v-if="avatarUrl" type="primary" disabled @click="onSave">
+        Save
+      </FormButton>
       <FormButton @click="$emit('cancel')"> Cancel </FormButton>
     </template>
   </DialogBase>
@@ -35,11 +39,11 @@
 
 <script setup lang="ts">
 import FormButton from '../common/form-button.vue';
+import ImageCropper from '../common/image-cropper.vue';
 import DialogBase from './dialog-base.vue';
 
 type ChangeAvatarDialogProps = {
-  avatarUrl: string;
-  displayName: string;
+  avatarUrl?: string;
   visible?: boolean;
 };
 
