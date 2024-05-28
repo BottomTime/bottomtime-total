5211b1aab1cfa4c5e496ca6e43c96fd07be0c385
Chris Carleton <mrchriscarleton@gmail.com>
2024-05-28 13:31:12 -0400

    Added new table for associating air tanks with log entries.

---
+++ packages/service/src/data/log-entry-air.entity.ts
@@ -0,0 +1,41 @@
+import { PressureUnit, TankMaterial } from '@bottomtime/api';
+
+import { Column, Entity, ManyToOne, PrimaryColumn } from 'typeorm';
+
+import { LogEntryEntity } from './log-entry.entity';
+
+@Entity('log_entry_air')
+export class LogEntryAirEntity {
+  @PrimaryColumn('uuid')
+  id: string = '';
+
+  @ManyToOne(() => LogEntryEntity, (logEntry) => logEntry.air)
+  logEntry: LogEntryEntity = new LogEntryEntity();
+
+  @Column({ type: 'varchar', length: 100 })
+  name: string = '';
+
+  @Column({ type: 'enum', enum: TankMaterial })
+  material: TankMaterial = TankMaterial.Aluminum;
+
+  @Column({ type: 'float' })
+  workingPressure: number = 0;
+
+  @Column({ type: 'float' })
+  volume: number = 0;
+
+  @Column({ type: 'float' })
+  startPressure: number = 0;
+
+  @Column({ type: 'float' })
+  endPressure: number = 0;
+
+  @Column({ type: 'enum', enum: PressureUnit })
+  pressureUnit: PressureUnit = PressureUnit.Bar;
+
+  @Column({ type: 'float', nullable: true })
+  o2Percent: number | null = null;
+
+  @Column({ type: 'float', nullable: true })
+  hePercent: number | null = null;
+}
