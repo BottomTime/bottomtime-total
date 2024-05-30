7ccd4806d53fa43e423323fbbd23d25ae1ebe503
Chris Carleton <mrchriscarleton@gmail.com>
2024-05-30 15:19:37 -0400

    Database can now be seeded with some pre-defined tanks.

--- packages/service/admin/database/seed-db.ts
+++ packages/service/admin/database/seed-db.ts
@@ -1,28 +1,9 @@
-import { UserRole } from '@bottomtime/api';
-
+/* eslint-disable no-console */
 import { DataSource } from 'typeorm';
 
-import { UserEntity } from '../../src/data';
+import { TankEntity } from '../../src/data';
 import { getDataSource } from './data-source';
-
-/* eslint-disable no-console */
-const DefaultAdminAccountId = '4a055f0a-0a9d-415d-8a36-93319e152055';
-const DefaultAdminEmail = 'admin@bottomti.me';
-const DefaultAdminUsername = 'Admin';
-const DefaultAdminPasswordHash =
-  '$2b$15$w/RXlIPZStVF.Y0LSs/q7e9UGtqQqct1eP.FrJEhXebN62SfN6KdC'; // "admin"
-const AdminUser: Partial<UserEntity> = {
-  id: DefaultAdminAccountId,
-  email: DefaultAdminEmail,
-  emailLowered: DefaultAdminEmail.toLowerCase(),
-  emailVerified: true,
-  isLockedOut: false,
-  memberSince: new Date(),
-  passwordHash: DefaultAdminPasswordHash,
-  role: UserRole.Admin,
-  username: DefaultAdminUsername,
-  usernameLowered: DefaultAdminUsername.toLocaleLowerCase(),
-};
+import TankData from './seed/tanks.json';
 
 export async function seedDatabase(postgresUri: string) {
   let ds: DataSource | undefined;
@@ -31,15 +12,9 @@ export async function seedDatabase(postgresUri: string) {
     console.log('Connecting to MongoDb...');
     ds = await getDataSource(postgresUri);
 
-    console.log('Creating admin user account...');
-    const users = ds.getRepository(UserEntity);
-
-    const admin = await users.existsBy({ id: DefaultAdminAccountId });
-    if (!admin) {
-      const adminUser = new UserEntity();
-      Object.assign(adminUser, AdminUser);
-      await users.save(adminUser);
-    }
+    console.log('Generating system tank profiles...');
+    const tanks = ds.getRepository(TankEntity);
+    await tanks.save(TankData as TankEntity[]);
 
     console.log('Database seeded.');
   } catch (error) {
