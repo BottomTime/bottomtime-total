b800d5af6de431caf407cdcd6f07ce0c64e1eabc
Chris Carleton <mrchriscarleton@gmail.com>
2024-08-07 12:10:35 -0400

    Added UX to allow users to manage their account type.

--- packages/web/src/components/users/manage-account.vue
+++ packages/web/src/components/users/manage-account.vue
@@ -78,50 +78,33 @@
         </div>
       </div>
 
-      <TextHeading>Account Type</TextHeading>
+      <TextHeading v-if="enableShopManagement.value">Account Type</TextHeading>
 
-      <FormField label="Account type">
-        <div class="pt-1.5">
-          <FormRadio
-            v-model="state.accountType"
-            control-id="acct-personal"
-            test-id="acct-personal"
-            group="acct-type"
-            value="personal"
-          >
-            Personal
-          </FormRadio>
-
-          <FormRadio
-            v-model="state.accountType"
-            control-id="acct-shopOwner"
-            test-id="acct-shopOwner"
-            group="acct-type"
-            value="shopOwner"
-          >
-            Shop Owner
-          </FormRadio>
-        </div>
-      </FormField>
+      <ManageMembership
+        v-if="enableShopManagement.value"
+        :user="user"
+        @account-type-changed="(type) => $emit('change-account-type', type)"
+      />
     </div>
   </form>
 </template>
 
 <script lang="ts" setup>
-import { UserDTO } from '@bottomtime/api';
+import { AccountTier, UserDTO } from '@bottomtime/api';
+import { ManageDiveOperatorsFeature } from '@bottomtime/common';
 
 import { onMounted, reactive } from 'vue';
 
 import { useClient } from '../../api-client';
 import { ToastType } from '../../common';
+import { useFeatureToggle } from '../../featrues';
 import { useOops } from '../../oops';
 import { useToasts } from '../../store';
 import FormButton from '../common/form-button.vue';
-import FormField from '../common/form-field.vue';
 import FormLabel from '../common/form-label.vue';
-import FormRadio from '../common/form-radio.vue';
 import TextHeading from '../common/text-heading.vue';
 import AccountTimestamps from './account-timestamps.vue';
+import ManageMembership from './manage-membership.vue';
 import ManagePassword from './manage-password.vue';
 import UsernameAndEmail from './username-and-email.vue';
 
@@ -140,8 +123,9 @@ type OAuthProvider = {
 type ManageAccountState = {
   isLoadingProviders: boolean;
   providers: OAuthProvider[];
-  accountType: 'personal' | 'shopOwner';
 };
+
+const enableShopManagement = useFeatureToggle(ManageDiveOperatorsFeature);
 
 const client = useClient();
 const oops = useOops();
@@ -172,13 +156,13 @@ const OAuthProviders: Omit<OAuthProvider, 'connected'>[] = [
 // STATE MANAGEMENT
 const props = defineProps<ManageAccountProps>();
 defineEmits<{
+  (e: 'change-account-type', accountTier: AccountTier): void;
   (e: 'change-username', username: string): void;
   (e: 'change-email', email: string): void;
   (e: 'change-password'): void;
 }>();
 
 const state = reactive<ManageAccountState>({
-  accountType: 'personal',
   isLoadingProviders: true,
   providers: [],
 });
---
+++ packages/web/src/components/users/manage-membership.vue
@@ -0,0 +1,201 @@
+<template>
+  <DrawerPanel
+    title="Change Account Type"
+    :visible="state.showChangeAccountType"
+    @close="onCancelChangeAccountType"
+  >
+    <form @submit.prevent="">
+      <fieldset class="px-4 space-y-6" :disabled="state.isSaving">
+        <div>
+          <FormRadio
+            v-model="state.accountTier"
+            control-id="account-tier-basic"
+            test-id="account-tier-basic"
+            group="account-tier"
+            :value="AccountTier.Basic.toString()"
+          >
+            <p class="flex items-baseline gap-3">
+              <span class="font-bold text-lg">Free account</span>
+              <span class="font-mono text-xs">( $0 forever! )</span>
+            </p>
+          </FormRadio>
+
+          <div class="space-y-3 pl-8 text-justify">
+            <p>
+              Get all of the features of BottomTime for personal use - for free!
+            </p>
+
+            <ul class="list-outside list-disc ml-6">
+              <li>Upload and log your dives</li>
+              <li>
+                Track your dive metrics and history in your personal dashboard
+              </li>
+              <li>Find dive sites and dive shops</li>
+              <li>Follow your dive buddies and</li>
+              <li>Earn XP and level up</li>
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <FormRadio
+            v-model="state.accountTier"
+            control-id="account-tier-shop-owner"
+            test-id="account-tier-shop-owner"
+            group="account-tier"
+            :value="AccountTier.ShopOwner.toString()"
+          >
+            <p class="flex items-baseline gap-3">
+              <span class="font-bold text-lg">Dive shop owner</span>
+              <span class="font-mono text-xs">( $40 / year )</span>
+            </p>
+          </FormRadio>
+
+          <div class="space-y-3 pl-8 text-justify">
+            <p>
+              Get all of the benefits of a free account, plus the ability to
+              register and manage your dive shop on our site.
+            </p>
+
+            <ul class="list-outside list-disc ml-6">
+              <li>
+                Register and manage your shop's profile including links to your
+                socials and website
+              </li>
+              <li>Upload images and videos</li>
+              <li>
+                Reach new divers by having your shop appear in search results
+                for dive sites
+              </li>
+              <li>
+                Tag dive sites that your shop visits so that divers will be
+                alerted when the sites appear in their search
+              </li>
+              <li>And more!</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="flex justify-center gap-3">
+          <FormButton
+            type="primary"
+            :is-loading="state.isSaving"
+            :disabled="state.accountTier === props.user.accountTier.toString()"
+            test-id="btn-change-account-type"
+            submit
+            @click="onConfirmChangeAccountType"
+          >
+            Change account type
+          </FormButton>
+          <FormButton
+            test-id="btn-cancel-account-type"
+            @click="onCancelChangeAccountType"
+          >
+            Cancel
+          </FormButton>
+        </div>
+      </fieldset>
+    </form>
+  </DrawerPanel>
+
+  <FormField label="Account type">
+    <fieldset
+      class="flex flex-col lg:flex-row gap-2 lg:gap-4 items-baseline"
+      :disabled="state.isSaving"
+    >
+      <div class="grow w-full">
+        <span data-testid="account-tier-value">
+          {{ accountTier }}
+        </span>
+      </div>
+
+      <div class="min-w-36 lg:min-w-40 xl:min-w-48">
+        <FormButton
+          test-id="change-account-type"
+          stretch
+          @click="onChangeAccountType"
+        >
+          Change account type...
+        </FormButton>
+      </div>
+    </fieldset>
+  </FormField>
+</template>
+
+<script lang="ts" setup>
+import { AccountTier, UserDTO } from '@bottomtime/api';
+
+import { computed, reactive } from 'vue';
+
+import { useClient } from '../../api-client';
+import { ToastType } from '../../common';
+import { useOops } from '../../oops';
+import { useToasts } from '../../store';
+import DrawerPanel from '../common/drawer-panel.vue';
+import FormButton from '../common/form-button.vue';
+import FormField from '../common/form-field.vue';
+import FormRadio from '../common/form-radio.vue';
+
+interface ManageAccountProps {
+  user: UserDTO;
+}
+
+interface ManageAccountState {
+  accountTier: string;
+  isSaving: boolean;
+  showChangeAccountType: boolean;
+}
+
+const client = useClient();
+const oops = useOops();
+const toasts = useToasts();
+
+const props = defineProps<ManageAccountProps>();
+const state = reactive<ManageAccountState>({
+  accountTier: props.user.accountTier.toString(),
+  isSaving: false,
+  showChangeAccountType: false,
+});
+const emit = defineEmits<{
+  (e: 'account-type-changed', accountTier: AccountTier): void;
+}>();
+
+const accountTier = computed<string>(() => {
+  switch (props.user.accountTier) {
+    default:
+    case AccountTier.Basic:
+      return 'Free account';
+    case AccountTier.ShopOwner:
+      return 'Dive shop owner';
+  }
+});
+
+function onChangeAccountType() {
+  state.showChangeAccountType = true;
+}
+
+function onCancelChangeAccountType() {
+  state.showChangeAccountType = false;
+}
+
+async function onConfirmChangeAccountType(): Promise<void> {
+  state.isSaving = true;
+
+  await oops(async () => {
+    const newAccountTier = parseInt(state.accountTier, 10) as AccountTier;
+    const user = client.users.wrapDTO(props.user);
+
+    await user.changeMembership(newAccountTier);
+
+    toasts.toast({
+      id: 'account-type-changed',
+      message: 'Account type changed successfully!',
+      type: ToastType.Success,
+    });
+    emit('account-type-changed', newAccountTier);
+    state.showChangeAccountType = false;
+  });
+
+  state.isSaving = false;
+}
+</script>
