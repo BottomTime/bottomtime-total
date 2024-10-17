462eb8015b854eb6ff69c6bf966511471ad6aaf7
Chris Carleton <mrchriscarleton@gmail.com>
2024-10-17 08:06:22 -0400

    Finished testing the dive operators view.

--- packages/web/tests/spec/views/dive-operators-view.spec.ts
+++ packages/web/tests/spec/views/dive-operators-view.spec.ts
@@ -5,6 +5,7 @@ import {
   DiveOperator,
   DiveOperatorDTO,
   Fetcher,
+  HttpException,
   SearchDiveOperatorsResponseDTO,
   SearchDiveOperatorsResponseSchema,
   UserDTO,
@@ -22,10 +23,16 @@ import { Pinia, createPinia } from 'pinia';
 import { Router } from 'vue-router';
 
 import { ApiClientKey } from '../../../src/api-client';
+import { ToastType } from '../../../src/common';
 import DiveOperatorsListItem from '../../../src/components/operators/dive-operators-list-item.vue';
 import EditDiveOperator from '../../../src/components/operators/edit-dive-operator.vue';
+import ViewDiveOperator from '../../../src/components/operators/view-dive-operator.vue';
 import { FeaturesServiceKey } from '../../../src/featrues';
-import { useCurrentUser, useDiveOperators } from '../../../src/store';
+import {
+  useCurrentUser,
+  useDiveOperators,
+  useToasts,
+} from '../../../src/store';
 import DiveOperatorsView from '../../../src/views/dive-operators-view.vue';
 import { ConfigCatClientMock } from '../../config-cat-client-mock';
 import { createRouter } from '../../fixtures/create-router';
@@ -42,10 +49,10 @@ describe('Dive operators view', () => {
   let pinia: Pinia;
   let currentUser: ReturnType<typeof useCurrentUser>;
   let diveOperators: ReturnType<typeof useDiveOperators>;
+  let toasts: ReturnType<typeof useToasts>;
   let opts: ComponentMountingOptions<typeof DiveOperatorsView>;
 
   beforeAll(() => {
-    testData = SearchDiveOperatorsResponseSchema.parse(TestData);
     fetcher = new Fetcher();
     client = new ApiClient();
     router = createRouter([
@@ -58,10 +65,12 @@ describe('Dive operators view', () => {
   });
 
   beforeEach(async () => {
+    testData = SearchDiveOperatorsResponseSchema.parse(TestData);
     features.flags[ManageDiveOperatorsFeature.key] = true;
     pinia = createPinia();
     currentUser = useCurrentUser(pinia);
     diveOperators = useDiveOperators(pinia);
+    toasts = useToasts(pinia);
     await router.push('/shops');
     opts = {
       global: {
@@ -281,6 +290,208 @@ describe('Dive operators view', () => {
         wrapper.getComponent(DiveOperatorsListItem).props('operator'),
       ).toEqual(result);
       expect(wrapper.findComponent(EditDiveOperator).exists()).toBe(false);
+    });
+
+    it('will allow a shop owner to edit an existing shop', async () => {
+      const existing: DiveOperatorDTO = {
+        ...testData.operators[0],
+        owner: ShopOwner.profile,
+      };
+      const update: CreateOrUpdateDiveOperatorDTO = {
+        address: '1234 Main St',
+        description: 'A new dive shop',
+        name: 'New Shop',
+        phone: '555-555-5555',
+        slug: existing.slug,
+        gps: existing.gps,
+        email: existing.email,
+        socials: existing.socials,
+        website: existing.website,
+      };
+      currentUser.user = ShopOwner;
+      diveOperators.results.operators = [existing];
+      diveOperators.results.totalCount = 1;
+
+      const operator = new DiveOperator(fetcher, existing);
+      const saveSpy = jest.spyOn(operator, 'save').mockResolvedValue();
+      jest.spyOn(client.diveOperators, 'wrapDTO').mockReturnValue(operator);
+
+      const wrapper = mount(DiveOperatorsView, opts);
+      await flushPromises();
+
+      const item = wrapper.getComponent(DiveOperatorsListItem);
+      item.get(`[data-testid="select-${existing.slug}"]`).trigger('click');
+      await flushPromises();
+
+      const editor = wrapper.getComponent(EditDiveOperator);
+      editor.vm.$emit('save', update);
+      await flushPromises();
+
+      expect(operator.toJSON()).toEqual({
+        ...existing,
+        ...update,
+      });
+      expect(saveSpy).toHaveBeenCalled();
+      expect(wrapper.findComponent(EditDiveOperator).exists()).toBe(false);
+    });
+
+    it('will handle a conflict error when saving an operator', async () => {
+      const existing: DiveOperatorDTO = {
+        ...testData.operators[0],
+        owner: ShopOwner.profile,
+      };
+      const update: CreateOrUpdateDiveOperatorDTO = {
+        address: '1234 Main St',
+        description: 'A new dive shop',
+        name: 'New Shop',
+        phone: '555-555-5555',
+        slug: existing.slug,
+        gps: existing.gps,
+        email: existing.email,
+        socials: existing.socials,
+        website: existing.website,
+      };
+      currentUser.user = ShopOwner;
+      diveOperators.results.operators = [existing];
+      diveOperators.results.totalCount = 1;
+
+      const operator = new DiveOperator(fetcher, existing);
+      const saveSpy = jest.spyOn(operator, 'save').mockRejectedValue(
+        new HttpException(409, 'Conflict', 'Nope', {
+          message: 'Nope',
+          method: 'PUT',
+          path: `/shops/${existing.slug}`,
+          status: 409,
+        }),
+      );
+      jest.spyOn(client.diveOperators, 'wrapDTO').mockReturnValue(operator);
+
+      const wrapper = mount(DiveOperatorsView, opts);
+      await flushPromises();
+
+      const item = wrapper.getComponent(DiveOperatorsListItem);
+      item.get(`[data-testid="select-${existing.slug}"]`).trigger('click');
+      await flushPromises();
+
+      const editor = wrapper.getComponent(EditDiveOperator);
+      editor.vm.$emit('save', update);
+      await flushPromises();
+
+      expect(operator.toJSON()).toEqual({
+        ...existing,
+        ...update,
+      });
+      expect(saveSpy).toHaveBeenCalled();
+      expect(wrapper.findComponent(EditDiveOperator).isVisible()).toBe(true);
+
+      expect(toasts.toasts).toHaveLength(1);
+      expect(toasts.toasts[0].type).toBe(ToastType.Warning);
+      expect(toasts.toasts[0].message).toBe(
+        'Unable to save dive shop. URL slug is already in use. Please change your slug to make it unique and then try again.',
+      );
+    });
+
+    it('will display a dive shop in read-only mode when viewed by a user who is not the owner', async () => {
+      currentUser.user = BasicUser;
+      const operator = testData.operators[0];
+      diveOperators.results.operators = [operator];
+      diveOperators.results.totalCount = 1;
+
+      const wrapper = mount(DiveOperatorsView, opts);
+      await flushPromises();
+
+      await wrapper
+        .get(`[data-testid="select-${operator.slug}"]`)
+        .trigger('click');
+      const viewer = wrapper.getComponent(ViewDiveOperator);
+      expect(viewer.isVisible()).toBe(true);
+      expect(viewer.props('operator')).toEqual(operator);
+    });
+
+    it('will allow user to load more results', async () => {
+      diveOperators.results.operators = testData.operators.slice(0, 10);
+      diveOperators.results.totalCount = 20;
+
+      const wrapper = mount(DiveOperatorsView, opts);
+      await flushPromises();
+
+      const spy = jest
+        .spyOn(client.diveOperators, 'searchDiveOperators')
+        .mockResolvedValue({
+          operators: testData.operators
+            .slice(10, 20)
+            .map((op) => new DiveOperator(fetcher, op)),
+          totalCount: 20,
+        });
+
+      await wrapper.get('[data-testid="operators-load-more"]').trigger('click');
+      await flushPromises();
+
+      expect(spy).toHaveBeenCalledWith({
+        skip: 10,
+      });
+      expect(diveOperators.results.operators).toHaveLength(20);
+      expect(diveOperators.results.operators).toEqual(
+        testData.operators.slice(0, 20),
+      );
+
+      const items = wrapper.findAllComponents(DiveOperatorsListItem);
+      expect(items).toHaveLength(20);
+      items.forEach((item, i) => {
+        expect(item.props('operator')).toEqual(testData.operators[i]);
+      });
+    });
+
+    it('will retain search criteria while performing "load more" action', async () => {
+      const query = {
+        query: 'deep site',
+        location: '20.42,-87.32',
+        radius: 350,
+        owner: 'the_dude',
+      };
+      await router.replace({
+        path: '/shops',
+        query,
+      });
+
+      diveOperators.results.operators = testData.operators.slice(0, 10);
+      diveOperators.results.totalCount = 20;
+
+      const wrapper = mount(DiveOperatorsView, opts);
+      await flushPromises();
+
+      const spy = jest
+        .spyOn(client.diveOperators, 'searchDiveOperators')
+        .mockResolvedValue({
+          operators: testData.operators
+            .slice(10, 20)
+            .map((op) => new DiveOperator(fetcher, op)),
+          totalCount: 20,
+        });
+
+      wrapper.get('[data-testid="operators-load-more"]').trigger('click');
+      await flushPromises();
+
+      expect(spy).toHaveBeenCalledWith({
+        location: {
+          lat: 20.42,
+          lon: -87.32,
+        },
+        owner: 'the_dude',
+        query: 'deep site',
+        radius: 350,
+        skip: 10,
+      });
+      expect(diveOperators.results.operators).toHaveLength(20);
+      expect(diveOperators.results.operators).toEqual(
+        testData.operators.slice(0, 20),
+      );
+
+      const items = wrapper.findAllComponents(DiveOperatorsListItem);
+      expect(items).toHaveLength(20);
+      items.forEach((item, i) => {
+        expect(item.props('operator')).toEqual(testData.operators[i]);
+      });
     });
   });
 });
